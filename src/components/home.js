07d0ff6b27b2273444fcfb9bdd7e7fe780150cf1
Evelyn V Pírez <evepirez@gmail.com>
2023-08-31 14:28:14 -0400

    modifiaciones de Home.js y Css

--- src/components/home.js
+++ src/components/home.js
@@ -2,17 +2,32 @@ function home(navigateTo) {
   const section = document.createElement('section');
   const title = document.createElement('h2');
   const button = document.createElement('button');
+  const slogan = document.createElement('p');
+  const logo = document.createElement('img');
+  const chevron = document.createElement('img');
 
-  button.textContent = 'login';
-  title.textContent = 'Welcome to green';
+  logo.src = './assets/re+.png';
+  logo.alt = 'logo de re+';
+  logo.className = 'logoIcon';
+  chevron.src = './assets/chevron.png';
+  chevron.alt = 'flecha hacia abajo';
+  chevron.className = 'chevronIcon';
+  button.textContent = 'Entrar';
+  title.textContent = 'ACERCA DE';
+  title.className = 'acercaDe';
+  slogan.textContent = 'Conectando personas por un mundo más sostenible';
+  slogan.className = 'slogan';
 
   button.addEventListener('click', () => {
     navigateTo('/login');
   });
 
-  section.append(title);
+  section.append(logo);
+  section.append(slogan);
   section.append(button);
+  section.append(title);
+  section.append(chevron);
   return section;
 }
 
-export default home;
+// export default home;
