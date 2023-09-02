22fbec727d52abb34db2c5dbe9f08bdef7067ba7
Rocio Lorca <lorcavillalobos@gmail.com>
2023-09-02 10:18:07 -0400

    Se realizan pruebas. Se comenta todo para ir verificando individualmente.

--- src/components/login.js
+++ src/components/login.js
@@ -1,66 +1,66 @@
-import { sigInWithGoogle, loginUser } from '../lib/index.js';
+// import { sigInWithGoogle, loginUser } from '../lib/index.js';
 
-function login(navigateTo) {
-  const section = document.createElement('section');
-  // const title = document.createElement('h2');
-  const buttonReturn = document.createElement('button');
-  const googleLogin = document.createElement('img');
-  // const button = document.createElement('button');
-  const form = document.createElement('form');
-  const inputEmail = document.createElement('input');
-  const inputPass = document.createElement('input');
-  const forgotPassword = document.createElement('p');
-  const newLogin = document.createElement('p');
-  const logoLogin = document.createElement('img');
-  const buttonLogin = document.createElement('button');
-  const registrarButton = document.createElement('button');
-  const buttonLoginUser = document.createElement('button');
+// function login(navigateTo) {
+//   const section = document.createElement('section');
+//   // const title = document.createElement('h2');
+//   const buttonReturn = document.createElement('button');
+//   const googleLogin = document.createElement('img');
+//   // const button = document.createElement('button');
+//   const form = document.createElement('form');
+//   const inputEmail = document.createElement('input');
+//   const inputPass = document.createElement('input');
+//   const forgotPassword = document.createElement('p');
+//   const newLogin = document.createElement('p');
+//   const logoLogin = document.createElement('img');
+//   const buttonLogin = document.createElement('button');
+//   const registrarButton = document.createElement('button');
+//   const buttonLoginUser = document.createElement('button');
 
-  logoLogin.src = './assets/re+.png';
-  logoLogin.alt = 'logo de re+';
-  logoLogin.className = 'logoIconLogin';
+//   logoLogin.src = './assets/re+.png';
+//   logoLogin.alt = 'logo de re+';
+//   logoLogin.className = 'logoIconLogin';
 
-  googleLogin.className = 'googleLogin';
-  googleLogin.src = './assets/googleLogin.png';
-  googleLogin.alt = 'Ingreso con google';
-  inputEmail.placeholder = 'Correo electrónico';
-  inputPass.placeholder = 'Contraseña';
+//   googleLogin.className = 'googleLogin';
+//   googleLogin.src = './assets/googleLogin.png';
+//   googleLogin.alt = 'Ingreso con google';
+//   inputEmail.placeholder = 'Correo electrónico';
+//   inputPass.placeholder = 'Contraseña';
 
-  buttonLogin.textContent = 'google';
-  buttonReturn.textContent = 'Return to home';
-  registrarButton.textContent = 'Registrar';
-  buttonLoginUser.textContent = 'Login';
+//   buttonLogin.textContent = 'google';
+//   buttonReturn.textContent = 'Return to home';
+//   registrarButton.textContent = 'Registrar';
+//   buttonLoginUser.textContent = 'Login';
 
-  // title.textContent = 'Login';
-  buttonReturn.textContent = 'ENTRAR';
-  newLogin.textContent = '¿Nuev@? Regístrate aquí.';
-  newLogin.className = 'newLogin';
-  forgotPassword.textContent = '¿Olvidaste tu contraseña?';
-  forgotPassword.className = 'forgotPassword';
+//   // title.textContent = 'Login';
+//   buttonReturn.textContent = 'ENTRAR';
+//   newLogin.textContent = '¿Nuev@? Regístrate aquí.';
+//   newLogin.className = 'newLogin';
+//   forgotPassword.textContent = '¿Olvidaste tu contraseña?';
+//   forgotPassword.className = 'forgotPassword';
 
-  // button.textContent = 'Return to home';
-  buttonReturn.addEventListener('click', () => {
-    navigateTo('/');
-  });
-  buttonLogin.addEventListener('click', sigInWithGoogle);
-  registrarButton.addEventListener('click', () => {
-    navigateTo('/registrar');
-  });
+//   // button.textContent = 'Return to home';
+//   buttonReturn.addEventListener('click', () => {
+//     navigateTo('/');
+//   });
+//   buttonLogin.addEventListener('click', sigInWithGoogle);
+//   registrarButton.addEventListener('click', () => {
+//     navigateTo('/registrar');
+//   });
 
-  buttonLoginUser.addEventListener('click', (event) => {
-    event.preventDefault();
-    const email = inputEmail.value;
-    const password = inputPass.value;
-    loginUser(email, password);
-  });
+//   buttonLoginUser.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const email = inputEmail.value;
+//     const password = inputPass.value;
+//     loginUser(email, password);
+//   });
 
-  section.append(logoLogin, googleLogin);
-  form.append(inputEmail, inputPass, buttonLoginUser);
-  section.append(form, buttonReturn, registrarButton, buttonLogin);
-  section.append(newLogin);
-  section.append(forgotPassword);
+//   section.append(logoLogin, googleLogin);
+//   form.append(inputEmail, inputPass, buttonLoginUser);
+//   section.append(form, buttonReturn, registrarButton, buttonLogin);
+//   section.append(newLogin);
+//   section.append(forgotPassword);
 
-  return section;
-}
+//   return section;
+// }
 
-export default login;
+// export default login;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-// Este es el punto de entrada de tu aplicacion
+import home from './components/home.js';
+// import login from './components/login.js';
+import error from './components/error.js';
+// import registrar from './components/registrar.js';
+
+const root = document.getElementById('root');
+
+const onNavigate = (pathname) => {
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+
+  while (root.firstChild) {
+    root.removeChild(root.firstChild);
+  }
+
+  root.appendChild(rutas[pathname]);
+};
+
+const rutas = {
+  '/': home(onNavigate),
+  // '/login': login(onNavigate),
+  '/error': error(onNavigate),
+  // '/registrar': registrar(onNavigate),
+  // '/FotoPerfil': FotoPerfil(onNavigate),
+  // '/PerfilUsuario': PerfilUsuario(onNavigate),
+};
+
+const component = () => rutas[window.location.pathname];
 
 window.onpopstate = () => {
   while (root.firstChild) {
