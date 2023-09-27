2610b663bb823a6b68ea6ab79e15d20bd3b4822a
RoLV <lorcavillalobos@gmail.com>
2023-09-27 13:23:26 -0300

    Compila todo

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,10 +5,11 @@ import {
   sendEmailVerification,
   signInWithEmailAndPassword,
   updateProfile,
+  signOut,
 } from 'firebase/auth';
 
 import {
-  addDoc, collection, Timestamp, getDocs, query, orderBy,
+  addDoc, collection, Timestamp, getDocs, query, orderBy, updateDoc, doc, deleteDoc,
 } from 'firebase/firestore';
 
 import { db, auth } from '../firebase';
@@ -76,6 +77,7 @@ const loginUser = (email, password, element) => signInWithEmailAndPassword(auth,
 
 const addPost = async (title, post) => {
   const name = auth.currentUser.displayName;
+  const id = auth.currentUser.uid;
   const date = Timestamp.now().toDate().toLocaleString();
   const postsCollection = collection(db, 'posts');
   await addDoc(postsCollection, {
@@ -83,6 +85,7 @@ const addPost = async (title, post) => {
     date,
     title,
     post,
+    id,
   });
 };
 
@@ -101,8 +104,7 @@ const showPosts = async (array) => {
   const individualPost = document.createElement('section');
   individualPost.className = 'individualPost';
   array.forEach((post) => {
-    const onePost = document.createElement('section');
-    onePost.className = 'individualPost';
+    const postContainer = document.createElement('section');
     const postName = document.createElement('p');
     postName.textContent = post.name;
     const postDate = document.createElement('p');
@@ -111,8 +113,8 @@ const showPosts = async (array) => {
     postTitle.textContent = post.title;
     const postContent = document.createElement('p');
     postContent.textContent = post.post;
-    onePost.append(postTitle, postName, postDate, postContent);
-    individualPost.append(onePost);
+    postContainer.append(postTitle, postName, postDate, postContent);
+    individualPost.appendChild(postContainer);
   });
   return individualPost;
 };
@@ -128,6 +130,30 @@ const updateDisplayName = async (newDisplayName) => {
   }
 };
 
+const obtainUserInfo = () => {
+  const user = auth.currentUser;
+  const name = user.displayName;
+  const email = user.email;
+  const id = user.uid;
+  const photo = user.photoURL;
+  const userInfo = {
+    name,
+    email,
+    id,
+    photo,
+  };
+  return userInfo;
+};
+
+const signOutUser = () => async () => {
+  try {
+    await signOut(auth);
+    return true;
+  } catch (error) {
+    return error;
+  }
+};
+
 export {
   sigInWithGoogle,
   createUser,
@@ -137,4 +163,6 @@ export {
   getPosts,
   showPosts,
   updateDisplayName,
+  obtainUserInfo,
+  signOutUser,
 };
