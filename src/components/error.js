c7918ac9625d993f4089d0881b7b5c3360c2b844
RoLV <lorcavillalobos@gmail.com>
2023-10-03 14:10:59 -0300

    Ajuste de estilos en CSS

--- src/components/error.js
+++ src/components/error.js
@@ -1,9 +1,9 @@
 function error() {
   const section = document.createElement('section'); // se crea sección para que sea un valor real, cree y devuelva un elemento 
-  const title = document.createElement('h2');
+  const title = document.createElement('p');
+  title.className = 'slogan';
   title.textContent = 'Error 404 page no found, please go home';
-  section.appendChild(title)
-  //creando un componente de error que contiene un elemento <section> y un título <h2>
+  section.appendChild(title);
   return title;
 }
 
